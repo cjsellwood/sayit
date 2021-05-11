ea471513a0e5ed5efc5b8f179a770ae31b3b5094
cjsellwood <c.sellwood22@gmail.com>
2021-05-11 14:39:30 +1000

    Added sidebar details on topic posts page

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import * as actions from "../store/actions/index";
 import { connect } from "react-redux";
@@ -10,10 +10,29 @@ const Topic = (props) => {
   useEffect(() => {
     props.onGetTopicPosts(topic);
 
-    // eslint-disable-next-line
-  }, [topic])
+    // Find index of topic in state
+    const topicIndex = props.topics.findIndex(
+      (topicState) => topicState.name === topic
+    );
 
-  const postsDisplay = props.posts.map(post => {
+    // If topic not in state, fetch and add it
+    if (topicIndex === -1) {
+      props.onAddTopic(topic);
+    }
+
+    // If post defined and topic in state, display topic in sidebar
+    if (topicIndex !== -1) {
+      props.onSetSidebar(
+        false,
+        props.topics[topicIndex].name,
+        props.topics[topicIndex].description
+      );
+    }
+
+    // eslint-disable-next-line
+  }, [topic, props.topics]);
+
+  const postsDisplay = props.posts.map((post) => {
     return (
       <li key={post.post_id}>
         <div to="/">
@@ -28,22 +47,21 @@ const Topic = (props) => {
           </p>
         </div>
       </li>
-    )
-  })
+    );
+  });
 
   return (
     <section>
       <h1>{topic}</h1>
-      <ul>
-        {postsDisplay}
-      </ul>
+      <ul>{postsDisplay}</ul>
     </section>
   );
 };
 
 const mapStateToProps = (state) => {
   return {
-    posts: state.posts.posts
+    posts: state.posts.posts,
+    topics: state.posts.topics,
   };
 };
 
@@ -52,6 +70,12 @@ const mapDispatchToProps = (dispatch) => {
     onGetTopicPosts: (topic) => {
       dispatch(actions.getTopicPosts(topic));
     },
+    onAddTopic: (topic) => {
+      dispatch(actions.addTopic(topic))
+    },
+    onSetSidebar: (isHome, name, description) => {
+      dispatch(actions.setSidebar(isHome, name, description))
+    }
   };
 };
 
