e2454c03c3286386974d61fcfaea399b3d773b3c
cjsellwood <c.sellwood22@gmail.com>
2021-05-14 16:25:33 +1000

    Finished styling sidebar

--- src/components/helpers/Sidebar.js
+++ src/components/helpers/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import AuthShow from "./AuthShow";
@@ -7,66 +7,94 @@ import * as actions from "../../store/actions/index";
 import "./Sidebar.css";
 
 const Sidebar = (props) => {
+  const [search, setSearch] = useState("");
+
+  // Handle input in search bar
+  const searchInput = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // Handle search bar submission
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+
   return (
     <aside
       className={`sidebar ${props.show ? "open" : ""}`}
       onClick={props.onToggleSidebar}
     >
-      <form>
-        <div>
-          <label htmlFor="search">Search</label>
-          <input type="text" name="search" id="search" placeholder="search" />
+      <form onSubmit={handleSearchSubmit}>
+        <div className="search-bar">
+          <label htmlFor="search"></label>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="search"
+            value={search}
+            onChange={searchInput}
+          />
+          <button type="submit" aria-label="search">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+          </button>
         </div>
       </form>
-      <AuthHide>
-        <li>
-          <button
-            className="basic-button"
-            onClick={(e) => {
-              // e.stopPropagation();
-              props.onToggleRegisterModal();
-            }}
-            aria-label="register"
-          >
-            Register
-          </button>
-        </li>
-      </AuthHide>
-      <AuthHide>
-        <li>
-          <button
-            className="basic-button"
-            onClick={(e) => {
-              // e.stopPropagation();
-              props.onToggleLoginModal();
-            }}
-            aria-label="login"
-          >
-            Login
-          </button>
-        </li>
-      </AuthHide>
-      <AuthShow>
-        <li>
-          <button
-            className="basic-button"
-            onClick={props.onDeauthorize}
-            aria-label="logout"
-          >
-            Logout
-          </button>
-        </li>
-      </AuthShow>
-      <Link to="/topics">Topics</Link>
-      <AuthShow>
-        <Link to="/newpost">New Post</Link>
-        <br />
-        {props.sidebar.isHome ? <Link to="/newtopic">New Topic</Link> : null}
-        <br />
-      </AuthShow>
-
-      <h3>{props.sidebar.name}</h3>
-      <p>{props.sidebar.description}</p>
+      <div className="sidebar-links">
+        <Link to="/topics">Topics</Link>
+        <AuthShow>
+          <Link to="/newpost">New Post</Link>
+          {props.sidebar.isHome ? <Link to="/newtopic">New Topic</Link> : null}
+        </AuthShow>
+      </div>
+      <ul className="auth-links">
+        <AuthHide>
+          <li>
+            <button
+              className="basic-button"
+              onClick={(e) => {
+                // e.stopPropagation();
+                props.onToggleRegisterModal();
+              }}
+              aria-label="register"
+            >
+              Register
+            </button>
+          </li>
+        </AuthHide>
+        <AuthHide>
+          <li>
+            <button
+              className="basic-button"
+              onClick={(e) => {
+                // e.stopPropagation();
+                props.onToggleLoginModal();
+              }}
+              aria-label="login"
+            >
+              Login
+            </button>
+          </li>
+        </AuthHide>
+        <AuthShow>
+          <li>
+            <button
+              className="basic-button"
+              onClick={props.onDeauthorize}
+              aria-label="logout"
+            >
+              Logout
+            </button>
+          </li>
+        </AuthShow>
+      </ul>
+      <div className="sidebar-topic">
+        <h3>{props.sidebar.name}</h3>
+        <p>{props.sidebar.description}</p>
+      </div>
     </aside>
   );
 };
